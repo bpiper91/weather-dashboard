64113f82b03658c899ee5892804a2562e4c976d2
bpiper91 <bpiper91@gmail.com>
2022-07-05 17:32:16 -0400

    add handler for search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-console.log("script linked")
+// DOM ELEMENT SELECTOR VARIABLES
+var searchButton = document.querySelector("#submit");
+
+// FUNCTIONS
+
+// handle search button clicks
+var searchButtonHandler = function(event) {
+    if (!event.target.parent().children("#city-search").value()) {
+        return false;
+    } else {
+        var city = event.target.parent().children("#city-search").value();
+        getWeather(city);
+    };
+};
+
+// look up weather in searched city
+var getWeather = function(cityName) {
+
+};
+
+// listen for search button click
+searchButton.addEventListener("click",searchButtonHandler);
